70905edef38945ee9d1af0503332e9fa7046ecd6
Niels Horn <niels.horn@smartcommerce.de>
2023-01-06 12:05:56 +0100

    homePage + contoller + stylig

---
+++ src/main/java/com/smartcommerce/dnc/constant/CtrlConst.java
@@ -0,0 +1,13 @@
+package com.smartcommerce.dnc.constant;
+
+public class CtrlConst {
+
+	// urls
+
+	public static final String HOME_URL = "/";
+
+
+	// pages
+
+	public static final String HOME_PAGE = "homePage";
+}
---
+++ src/main/java/com/smartcommerce/dnc/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package com.smartcommerce.dnc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.smartcommerce.dnc.constant.CtrlConst;
+
+@Controller
+public class HomePageController {
+
+	@RequestMapping(value = CtrlConst.HOME_URL, method = RequestMethod.GET)
+	public String homePage() {
+		return CtrlConst.HOME_PAGE;
+	}
+}
